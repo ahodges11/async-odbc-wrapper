0b7efba8e7bc60d21a8b076b8e1f36cc784b59a4
Alex Hodges <alexander.hodges.github@gmail.com>
2020-10-05 05:20:56 +0200

    added statement and statement_base

---
+++ src/statement.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ahodges on 04/10/2020.
+//
+
+#ifndef AODBC_STATEMENT_HPP
+#define AODBC_STATEMENT_HPP
+
+#endif //AODBC_STATEMENT_HPP
---
+++ src/statement_base.hpp
@@ -0,0 +1,8 @@
+//
+// Created by ahodges on 04/10/2020.
+//
+
+#ifndef AODBC_STATEMENT_BASE_HPP
+#define AODBC_STATEMENT_BASE_HPP
+
+#endif   // AODBC_STATEMENT_BASE_HPP
