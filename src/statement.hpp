af8ac063ebb0b68e19114855582c47161633483d
Alex Hodges <alexander.hodges.github@gmail.com>
2020-10-05 05:23:17 +0200

    added basic functionality

--- src/statement.hpp
+++ src/statement.hpp
@@ -2,7 +2,52 @@
 // Created by ahodges on 04/10/2020.
 //
 
-#ifndef AODBC_STATEMENT_HPP
-#define AODBC_STATEMENT_HPP
+#pragma once
 
-#endif //AODBC_STATEMENT_HPP
+#include "sql_function_wrappers.hpp"
+#include "statement_base.hpp"
+#include "result_set.hpp"
+
+namespace aodbc
+{
+    struct connection;
+
+    struct statement : statement_base
+    {
+        friend struct connection;
+
+        statement()
+        : statement_base()
+        {
+        }
+
+
+        /// @brief Executes the provided sql_statement
+        /// @param sql_statement The sql_statement
+        /// @return Quantity of affected rows, -1 if none.
+        SQLLEN execute(std::string &sql_statement)
+        {
+            assert(valid());
+            // execute the query
+            sql_exec_direct(&handle_stmt_, sql_statement);
+
+            // get effected rows
+            SQLLEN row_count;
+            sql_row_count(&handle_stmt_,&row_count);
+            return row_count;
+        }
+
+        /// @brief Executes the provided sql_statement as a query, returning the results
+        /// @param sql_statement The sql_statement
+        /// @return Returns a result_set containing the data produced by the query
+        result_set execute_query(std::string &sql_statement)
+        {
+            auto result = result_set();
+            free_stmt_close(); // Close any existing cursor
+            sql_exec_direct(&handle_stmt_,sql_statement);
+
+        }
+
+    };
+
+}   // namespace aodbc
--- src/statement_base.hpp
+++ src/statement_base.hpp
@@ -2,7 +2,48 @@
 // Created by ahodges on 04/10/2020.
 //
 
-#ifndef AODBC_STATEMENT_BASE_HPP
-#define AODBC_STATEMENT_BASE_HPP
+#pragma once
 
-#endif   // AODBC_STATEMENT_BASE_HPP
+#include "config.hpp"
+
+namespace aodbc
+{
+    struct statement;
+    struct result_set;
+
+    struct statement_base
+    {
+        friend struct statement;
+        friend struct result_set;
+
+        bool valid() const { return handle_stmt_; }
+
+        auto get_max_rows() -> SQLULEN { return sql_get_max_rows(&handle_stmt_); }
+        auto set_max_rows(SQLULEN max_rows) -> void { sql_set_max_rows(&handle_stmt_, max_rows); }
+
+        auto get_query_timeout() -> SQLULEN { return sql_get_query_timeout(&handle_stmt_); }
+        auto set_query_timeout(SQLULEN seconds) -> void { sql_set_query_timeout(&handle_stmt_, seconds); }
+
+        auto free_stmt_close() -> void { sql_free_stmt(&handle_stmt_, SQL_CLOSE); }
+
+      protected:
+        SQLHSTMT *get_handle() { return &handle_stmt_; }
+
+      private:
+        statement_base()
+        : handle_stmt_(SQL_NULL_HANDLE)
+        {
+        }
+
+        ~statement_base()
+        {
+            if (valid())
+            {
+                sql_dealloc_stmt(&handle_stmt_);
+            }
+        }
+
+      private:
+        SQLHSTMT handle_stmt_;
+    };
+}   // namespace aodbc
