2d75d0d1e64a8bd23a528b08ebb1f1ac71e6bc96
Alex Hodges <alexander.hodges.github@gmail.com>
2020-11-20 08:01:37 +0100

    imported fmt

--- src/log.hpp
+++ src/log.hpp
@@ -5,14 +5,20 @@
 #pragma once
 
 #include <boost/log/trivial.hpp>
+#include <fmt/format.h>
 
 namespace aodbc
 {
+
+
     inline void log_trace(const char *msg) { BOOST_LOG_TRIVIAL(trace) << msg; }
     inline void log_debug(const char *msg) { BOOST_LOG_TRIVIAL(debug) << msg; }
     inline void log_info(const char *msg) { BOOST_LOG_TRIVIAL(info) << msg; }
     inline void log_warning(const char *msg) { BOOST_LOG_TRIVIAL(warning) << msg; }
     inline void log_error(const char *msg) { BOOST_LOG_TRIVIAL(error) << msg; }
     inline void log_fatal(const char *msg) { BOOST_LOG_TRIVIAL(fatal) << msg; }
+
+
+
 
 }   // namespace aodbc
