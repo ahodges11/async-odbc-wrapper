80f840334742b7ed299f00b8ed080e7e6d1d7748
Alex Hodges <alexander.hodges.github@gmail.com>
2020-11-20 08:01:57 +0100

    added global thead pool for async connections

---
+++ src/async/thread_pool.hpp
@@ -0,0 +1,18 @@
+//
+// Created by ahodges on 19/11/2020.
+//
+
+#pragma once
+
+#include "config.hpp"
+#include <thread>
+
+namespace aodbc::async
+{
+    inline net::thread_pool & get_thread_pool()
+    {
+        //static net::thread_pool p(std::max(std::thread::hardware_concurrency(),1u));
+        static net::thread_pool p(std::max(std::thread::hardware_concurrency(),50u));
+        return p;
+    }
+}
