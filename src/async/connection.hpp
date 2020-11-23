bb9eb5cb98bbc89f3457f6a1efc4e24c8f071563
Alex Hodges <alexander.hodges.github@gmail.com>
2020-11-23 16:19:11 +0100

    updated connection to remove segfaults. Updated interface to accept custom message handling location

--- src/async/connection.hpp
+++ src/async/connection.hpp
@@ -6,35 +6,48 @@
 
 #include "async/result_set/result_set_concept.hpp"
 #include "sync.hpp"
+#include "thread_pool.hpp"
 
 #include <boost/function.hpp>
 #include <boost/thread/concurrent_queues/sync_queue.hpp>
+#include <memory>
 #include <threads.h>
 
 namespace aodbc::async
 {
     namespace detail
     {
-
-        struct connection_wrapper
+        struct connection_wrapper : std::enable_shared_from_this< connection_wrapper >
         {
             connection_wrapper()
-            : thread_pool_(std::make_shared<net::thread_pool>(1))
+            : exec_(net::system_executor())
+            , connection_(std::make_shared< sync::connection >())
             {
             }
 
-            net::awaitable< void > connect(std::string &str)
+            ~connection_wrapper()
             {
-                co_await net::co_spawn(
-                    thread_pool_->get_executor(),
-                    [&]() -> net::awaitable< void > { co_return connection_.connect(str); },
-                    net::use_awaitable);
+                if (connection_->connected())
+                {
+                    net::dispatch(get_executor(), [conn = connection_]() { conn->disconnect(); });
+                }
+            }
+
+            net::awaitable< void > connect(std::string connection_str, std::vector<message> * messages)
+            {
+                auto work = [self = shared_from_this(),
+                             str  = std::move(connection_str), messages]() mutable -> net::awaitable< void > {
+                    co_return self->connection_->connect(str,messages);
+                };
+                co_await net::co_spawn(get_executor(), std::move(work), net::use_awaitable);
             }
             net::awaitable< void > disconnect()
             {
                 co_await net::co_spawn(
-                    thread_pool_->get_executor(),
-                    [&]() -> net::awaitable< void > { co_return connection_.disconnect(); },
+                    get_executor(),
+                    [self = shared_from_this()]() -> net::awaitable< void > {
+                        co_return self->connection_->disconnect();
+                    },
                     net::use_awaitable);
             }
 
@@ -42,64 +55,83 @@ namespace aodbc::async
             execute(std::string &sql_statement, std::size_t timeout = 0, std::size_t max_rows = 0)
             {
                 co_return co_await net::co_spawn(
-                    thread_pool_->get_executor(),
-                    [&]() -> net::awaitable< long > {
-                        co_return connection_.execute(sql_statement, timeout, max_rows);
+                    get_executor(),
+                    [self = shared_from_this(), sql_statement = std::move(sql_statement), timeout, max_rows]() mutable
+                    -> net::awaitable< long > {
+                        co_return self->connection_->execute(sql_statement, timeout, max_rows);
                     },
                     net::use_awaitable);
             }
 
             template < result_set::AsyncResultSet ResultSet >
-            net::awaitable< std::unique_ptr< ResultSet > >
-            execute_query(std::string &sql_statement, std::size_t timeout = 0, std::size_t max_rows = 0)
+            net::awaitable< std::unique_ptr< ResultSet > > execute_query(std::string &           sql_statement,
+                                                                         std::size_t             timeout  = 0,
+                                                                         std::size_t             max_rows = 0,
+                                                                         std::vector< message > *messages = nullptr)
             {
                 co_return std::make_unique< ResultSet >(
                     co_await net::co_spawn(
-                        thread_pool_->get_executor(),
-                        [&]() -> net::awaitable< std::unique_ptr<typename ResultSet::impl_type> > {
-                            co_return connection_.execute_query< typename ResultSet::impl_type >(
-                                sql_statement, timeout, max_rows);
+                        get_executor(),
+                        [self          = shared_from_this(),
+                         sql_statement = sql_statement,
+                         timeout,
+                         max_rows,
+                         messages]() mutable -> net::awaitable< std::unique_ptr< typename ResultSet::impl_type > > {
+                            co_return self->connection_->execute_query< typename ResultSet::impl_type >(
+                                sql_statement, timeout, max_rows, messages);
                         },
                         net::use_awaitable),
-                    thread_pool_);
+                    get_executor());
             }
 
           private:
-            // thread to do work on
-            std::shared_ptr< net::thread_pool > thread_pool_;
-            sync::connection                    connection_;
+            // net::strand< net::thread_pool::executor_type > get_executor()
+            net::strand< net::system_executor > get_executor()
+
+            {
+                assert(exec_.impl_);
+                return exec_;
+            }
+
+            // net::strand< net::thread_pool::executor_type > exec_;
+            net::strand< net::system_executor > exec_;
+            std::shared_ptr< sync::connection > connection_;
         };
     }   // namespace detail
 
     struct connection
     {
-        explicit connection(net::any_io_executor exec)
-        : exec_(exec)
+        explicit connection()
+        : impl_(std::make_shared< detail::connection_wrapper >())
         {
         }
 
         connection(const connection &other) = delete;
         connection &operator=(const connection &other) = delete;
 
-        net::awaitable< void > connect(std::string &connection_str) { co_await impl_.connect(connection_str); }
+        net::awaitable< void > connect(std::string connection_str, std::vector<message> * messages= nullptr)
+        {
+            co_await impl_->connect(std::move(connection_str), messages);
+        }
 
-        net::awaitable< void > disconnect() { co_await impl_.disconnect(); }
+        net::awaitable< void > disconnect() { co_await impl_->disconnect(); }
 
       public:   // execute statements
         net::awaitable< long > execute(std::string &sql_statement, std::size_t timeout = 0, std::size_t max_rows = 0)
         {
-            co_return co_await impl_.execute(sql_statement, timeout, max_rows);
+            co_return co_await impl_->execute(sql_statement, timeout, max_rows);
         }
 
         template < result_set::AsyncResultSet ResultSet >
-        net::awaitable< std::unique_ptr< ResultSet > >
-        execute_query(std::string &sql_statement, std::size_t timeout = 0, std::size_t max_rows = 0)
+        net::awaitable< std::unique_ptr< ResultSet > > execute_query(std::string &           sql_statement,
+                                                                     std::size_t             timeout  = 0,
+                                                                     std::size_t             max_rows = 0,
+                                                                     std::vector< message > *messages = nullptr)
         {
-            co_return co_await impl_.execute_query< ResultSet >(sql_statement, timeout, max_rows);
+            co_return co_await impl_->execute_query< ResultSet >(sql_statement, timeout, max_rows, messages);
         }
 
       private:
-        net::any_io_executor       exec_;
-        detail::connection_wrapper impl_;
+        std::shared_ptr< detail::connection_wrapper > impl_;
     };
 }   // namespace aodbc::async
